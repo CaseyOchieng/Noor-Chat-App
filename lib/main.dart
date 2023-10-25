91b9ba0ca12e3891a37b3d9e47c5fd5acb25a36f
casey <caseyochieng46@gmail.com>
2023-10-25 23:00:46 +0300

    fireBase added initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:noor/auth/login_or_register.dart';
+import 'package:noor/firebase_options.dart';
 import 'package:noor/theme/dark_mode.dart';
 import 'package:noor/theme/light_mode.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
