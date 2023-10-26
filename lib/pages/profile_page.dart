15c1212aa1143ed9effc9e0229b06c77e0a866bf
casey <caseyochieng46@gmail.com>
2023-10-26 15:56:18 +0300

    fixed the profile

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,16 +1,26 @@
+// ignore_for_file: must_be_immutable
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class ProfilePage extends StatefulWidget {
-  const ProfilePage({super.key});
+class ProfilePage extends StatelessWidget {
+  ProfilePage({super.key});
+  //current user logged in
+  User? currentUser = FirebaseAuth.instance.currentUser;
 
-  @override
-  State<ProfilePage> createState() => _ProfilePageState();
-}
+  // future fetch use data
+  Future<DocumentSnapshot<Map<String, dynamic>>> getUserDetails() async {
+    return await FirebaseFirestore.instance
+        .collection('Users')
+        .doc(currentUser!.email)
+        .get();
+  }
 
-class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
       appBar: AppBar(
         elevation: 0,
         title: Text(
@@ -20,6 +30,44 @@ class _ProfilePageState extends State<ProfilePage> {
           ),
         ),
       ),
+      body: FutureBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+          future: getUserDetails(),
+          builder: (context, snapshot) {
+            // Loading Circle
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            // error handling
+            else if (snapshot.hasError) {
+              return Center(
+                child: Text('Error: ${snapshot.error}'),
+              );
+            }
+            // data handling
+            else if (snapshot.hasData) {
+              //extracting data
+              Map<String, dynamic>? user = snapshot.data!.data();
+
+              return Column(
+                children: [
+                  // Profile pic
+                  Image.asset('asset/profile.png'),
+                  //Name of user
+                  Text(
+                    user!['username'].toString(),
+                    style: const TextStyle(),
+                  ),
+                  //Email
+                  Text(
+                    user['email'].toString(),
+                    style: const TextStyle(),
+                  )
+                ],
+              );
+            }
+          }),
     );
   }
 }
